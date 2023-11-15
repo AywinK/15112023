f4e76ea2556464f8a9635aa6e58e8a556324f915
Aywin <AywinK@hotmail.com>
2023-11-15 20:11:59 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 // import people from "./assets/people.json";
 import movies from "./assets/movies.json"
+import Movie from './components/Movie';
 
 function App() {
 
@@ -38,14 +39,7 @@ function App() {
         const { title, img, director, actorsArr, year, genre } = movies[movieKey];
 
         return (
-          <div key={movieKey} style={{ border: "0.2rem solid red", borderRadius: "32px", marginTop: "10px", maxWidth: "1000px", padding: "20px" }}>
-            <h2>{title}</h2>
-            <img src={`./${img}.png`} alt={img} width={400} height={500} style={{borderRadius: "24px"}} />
-            <p>Director: {director}</p>
-            <p>Actors: {actorsArr.join(", ")}</p>
-            <p>Year: {year}</p>
-            <p>Genre: {genre}</p>
-          </div>
+          <Movie key={title} title img director actorsArr year genre />
         )
       })}
 
