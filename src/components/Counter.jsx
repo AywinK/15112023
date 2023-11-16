736a2a78cb547235b7687828aa2818518fcde103
Aywin <AywinK@hotmail.com>
2023-11-16 20:42:17 +0000

    .

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -20,8 +20,8 @@ export default function Counter() {
     return (
         <div>
             <p>Count: {count}</p>
-            <button onClick={handleDecrementClick}>Decrement</button>
-            <button onClick={handleIncrementClick}>Increament</button>
+            <button disabled={(count > 0) ? false : true} onClick={handleDecrementClick}>Decrement</button>
+            <button disabled={(count >= 25) ? true : false} onClick={handleIncrementClick}>Increament</button>
         </div>
     )
 }
